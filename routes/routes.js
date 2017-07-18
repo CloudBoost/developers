317268727722f30e72436e3d55b4866049ce3ab1
shubhamqweasd <shubhamqweasd@gmail.com>
2017-07-18 11:56:55 +0530

    crash fix

--- routes/routes.js
+++ routes/routes.js
@@ -6,77 +6,77 @@ var fs = require('fs');
 var Q = require('q');
 var path = require('path');
 var request = require('request');
-var marked = require('marked'); 
+var marked = require('marked');
 
-router.get('/', function(req, res) {    
+router.get('/', function (req, res) {
 
-    var language="en";//English
-    var categoryName="gettingstarted";
-    var subCategoryName="yourfirstapp";
+  var language = "en";//English
+  var categoryName = "gettingstarted";
+  var subCategoryName = "yourfirstapp";
 
-    var promises=[];
-    promises.push(getTutorialDetails(language,categoryName,subCategoryName));
-    promises.push(getTutorialTopics());
+  var promises = [];
+  promises.push(getTutorialDetails(language, categoryName, subCategoryName));
+  promises.push(getTutorialTopics());
 
-    Q.all(promises).then(function(list){ 
+  Q.all(promises).then(function (list) {
 
-      res.render('index',{
-       language: language, 
-       category: categoryName,
-       subCategory: subCategoryName,
-       tutorialTopics: list[1],
-       tutorialDetails: list[0]
-     });
-    },function(error){
+    res.render('index', {
+      language: language,
+      category: categoryName,
+      subCategory: subCategoryName,
+      tutorialTopics: list[1],
+      tutorialDetails: list[0]
+    });
+  }, function (error) {
 
-      console.log(error);
-      render404(res);
-    });  
-
+    console.log(error);
+    render404(res);
   });
-
-router.get('/en/:category/:subcategory', function(req, res) {    
-
-    var language='en';
-    var categoryName=req.params.category;
-    var subCategoryName=req.params.subcategory;
-
-    var promises=[];
-    promises.push(getTutorialDetails(language,categoryName,subCategoryName));
-    promises.push(getTutorialTopics());
-
-    Q.all(promises).then(function(list){ 
-
-        res.render('index',{
-                   language: language, 
-                   category: categoryName,
-                subCategory: subCategoryName,
-             tutorialTopics: list[1],
-            tutorialDetails: list[0]
-                });
-    },function(error){
-        console.log(error);
-        return res.status(500).send(error);  
-    });  
 
 });
 
-router.get('*',function(req,res){
+router.get('/en/:category/:subcategory', function (req, res) {
 
+  var language = 'en';
+  var categoryName = req.params.category;
+  var subCategoryName = req.params.subcategory;
 
-    var promises=[];
-    promises.push(getTutorialTopics());
+  var promises = [];
+  promises.push(getTutorialDetails(language, categoryName, subCategoryName));
+  promises.push(getTutorialTopics());
 
-    Q.all(promises).then(function(list){ 
+  Q.all(promises).then(function (list) {
 
-        res.render('404',{
-                   tutorialTopics: list[1],
-            tutorialDetails: list[0]
-                });
-    },function(error){
-        console.log(error);
-        return res.status(500).send(error);  
+    res.render('index', {
+      language: language,
+      category: categoryName,
+      subCategory: subCategoryName,
+      tutorialTopics: list[1],
+      tutorialDetails: list[0]
     });
+  }, function (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  });
+
+});
+
+router.get('*', function (req, res) {
+
+
+  var promises = [];
+  promises.push(getTutorialTopics());
+
+  Q.all(promises).then(function (list) {
+
+    res.render('404', {
+      tutorialTopics: list[1],
+      tutorialDetails: list[0]
+    });
+  }, function (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  });
 
 });
 
@@ -84,46 +84,51 @@ module.exports = router
 
 /*********************************Private Functions**********************************/
 //getTutorials
-function getTutorialTopics(){
+function getTutorialTopics() {
   var deferred = Q.defer();
 
-  var url=global.keys.frontendServerUrl+"/tutorial";
+  var url = global.keys.frontendServerUrl + "/tutorial";
 
   request({
-    url: url,   
+    url: url,
     method: 'GET'
-  }, function(error, response, body){
-    if(error || response.statusCode==400 || response.statusCode==500) {
-     deferred.reject(error);       
-   } else {    
-    var info = JSON.parse(body);
-    
-    deferred.resolve(info);       
-  }
-});
+  }, function (error, response, body) {
+    if (error || response.statusCode == 400 || response.statusCode == 500) {
+      deferred.reject(error);
+    } else {
+      try {
+        var info = JSON.parse(body);
+
+        deferred.resolve(info);
+      } catch (e) {
+        deferred.reject(e);
+      }
+
+    }
+  });
 
   return deferred.promise;
 }
 
 //getTutorials
-function getTutorialDetails(language,categoryName,subCategoryName){
-  var deferred = Q.defer();  
+function getTutorialDetails(language, categoryName, subCategoryName) {
+  var deferred = Q.defer();
 
-  var uri='./tutorials/'+language+'/'+categoryName+'/'+subCategoryName+'.md';
+  var uri = './tutorials/' + language + '/' + categoryName + '/' + subCategoryName + '.md';
 
-  console.log("Tutorial requested:"+uri);  
+  console.log("Tutorial requested:" + uri);
 
-  fs.readFile(uri,'utf8', function (err,data) {
+  fs.readFile(uri, 'utf8', function (err, data) {
     if (err) {
       return deferred.reject(err);
-    }        
-    deferred.resolve(data);   
+    }
+    deferred.resolve(data);
   });
 
   return deferred.promise;
 }
 
 function render404(res) {
-  
+
   res.status(200).render('404');
 }
